643d917ac1348f8310d782d71e92949d47bd2764
Xnoe <xnoe@xnoe.moe>
2021-06-20 18:37:40 +0100

    Removed comment block from userconnection.cpp

--- userconnection.cpp
+++ userconnection.cpp
@@ -16,33 +16,6 @@ void UserConnection::process_cmd(std::string cmd_in) {
   IRCCommand cmd = IRCCommand(cmd_in);
   std::cout << cmd.to_string() << std::endl;
 
-/* if (state == Registered) {
-    user->process_cmd(cmd);
-    delete cmd;
-    return;
-  }
-
-  std::string command = cmd.get_command();
-
-  if (command == "NICK" && cmd.has_parameter()) {
-    user->set_nick(cmd.get_parameter(0));
-  }
-
-  if (command == "USER" && cmd.has_parameter()) {
-    user->set_username(cmd.get_parameter(0));
-    user->set_realname(cmd.get_parameter(3));
-  }
-  
-  if (user->has_nick() && user->has_username()) {
-    state = Registered;
-    send(":xnoircd 001 " + user->get_nick() + " :Welcome");
-    send(":xnoircd 002 " + user->get_nick() + " :Welcome");
-    send(":xnoircd 003 " + user->get_nick() + " :Welcome");
-    send(":xnoircd 004 " + user->get_nick() + " xnoircd 0 ABCabc ABCabc");
-    send(":xnoircd 422 " + user->get_nick() + " :No MOTD");
-  }
-*/
-
   for (IRCCommand c : user->process_cmd(cmd))
     send(c.to_string());
 }
